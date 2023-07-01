01927938be1fb6f06bc50264fab3f4e8b2a59650
Chris Kerwell Gresla <chris@lindy.ai>
2023-07-01 17:28:56 -0500

    updated the 1st sample w comments

--- SAMPLES/1-1.sh
+++ SAMPLES/1-1.sh
@@ -1,2 +1,10 @@
 # program to print out all employess who worked more than a single hour
 awk '$3 > 0 { print $1, $2 * $3 }' DATA/emp.data
+
+	# do a filter where field 3's values are greater than zero (more than 1 hr worked)
+	# print the name and pay (multiply hour by rate to get their pay) for that employee
+
+# This is a simple example of a pattern-action statement, where PATTERN is true do ACTION
+	# PATTERN = $3 > 0
+	#  ACTION = { print $1, $2 * $3 } 
+
