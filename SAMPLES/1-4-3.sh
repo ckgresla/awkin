fecb789f02abd7c0b883e2a20b7d03f4be473328
Chris Kerwell Gresla <chris@lindy.ai>
2023-07-09 12:49:58 -0700

    text content comparison example + find lines in all files wher regex match

---
+++ SAMPLES/1-4-3.sh
@@ -0,0 +1,9 @@
+# Selection by Text Equality
+
+
+awk '$1 == "Susie"' DATA/emp.data
+
+	# make sure to put the string to compare against inside double quotes!
+
+
+
---
+++ SAMPLES/1-4-4.sh
@@ -0,0 +1,6 @@
+# REGEX is supported, and with the ability to take mulitple files as input....
+# recreate GREP in awk
+
+awk '/Susie/' DATA/*
+
+	# Finds all lines in any file inside DATA that contains 'Susie'
